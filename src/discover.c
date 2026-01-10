58ea6afec418838fdcf3f2927ca661a1cbad15ca
Phandal <bfeld.111@gmail.com>
2026-01-09 21:15:00 -0600

    updated discover to work with all items on network

--- src/discover.c
+++ src/discover.c
@@ -1,14 +1,16 @@
-#include "frame.h"
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include "frame.h"
+
 #define BROADCAST "255.255.255.255"
-#define HOST "localhost"
+#define HOST "0.0.0.0"
 #define PORT 56700
-#define LOCAL_PORT 8080
 
 void payload_print(const lifx_payload_t *payload, lifx_message_type type) {
   if (payload == NULL) {
@@ -63,66 +65,6 @@ void frame_print(const lifx_frame_t *frame) {
   payload_print(&frame->payload, frame->header.type);
 }
 
-int start_server() {
-  int res, sfd;
-  struct sockaddr_in server_addr;
-  struct in_addr in_addr = {0};
-  inet_pton(AF_INET, HOST, &in_addr);
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr = in_addr;
-  server_addr.sin_port = htons(LOCAL_PORT);
-  sfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (sfd == -1) {
-    perror("socket");
-    return -1;
-  }
-
-  int yes = 1;
-  res = setsockopt(sfd, SOL_SOCKET, SO_BROADCAST, &yes, sizeof(yes));
-  if (res == -1) {
-    perror("setsockopt");
-    close(sfd);
-    return -1;
-  }
-
-  res = bind(sfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-  if (res == -1) {
-    perror("bind");
-    close(sfd);
-    return -1;
-  }
-
-  return sfd;
-}
-
-int send_packet(int sfd, lifx_frame_t *frame) {
-
-  uint8_t p[FRAME_HEADER_SIZE] = {0};
-  uint8_t *packet = p;
-  int size;
-  size = lifx_encode_frame(frame, &packet, FRAME_HEADER_SIZE);
-  if (size == -1) {
-    fprintf(stderr, "failed to encode packet\n");
-    return -1;
-  }
-
-  int res;
-  struct in_addr in_addr;
-  inet_pton(AF_INET, BROADCAST, &in_addr);
-  struct sockaddr_in addr = {0};
-  addr.sin_family = AF_INET;
-  addr.sin_addr = in_addr;
-  addr.sin_port = htons(PORT);
-
-  res = sendto(sfd, packet, size, 0, (struct sockaddr *)&addr, sizeof(addr));
-  if (res == -1) {
-    perror("sendto");
-    return -1;
-  }
-
-  return res;
-}
-
 int get_lights(int sfd, lifx_frame_t **frame, int n) {
   if (frame == NULL) {
     return -1;
@@ -154,6 +96,26 @@ int get_lights(int sfd, lifx_frame_t **frame, int n) {
 }
 
 int main(void) {
+  struct in_addr ip;
+  if (inet_pton(AF_INET, HOST, &ip) != 1) {
+    fprintf(stderr, "failed to convert ip address to network '%s'\n", HOST);
+    exit(EXIT_FAILURE);
+  }
+  struct sockaddr_in addr = {0};
+  addr.sin_family = AF_INET;
+  addr.sin_addr = ip;
+
+  struct in_addr b_addr;
+  if (inet_pton(AF_INET, BROADCAST, &b_addr) != 1) {
+    fprintf(stderr, "failed to convert ip address to network '%s'\n",
+            BROADCAST);
+    exit(EXIT_FAILURE);
+  }
+  struct sockaddr_in broadcast = {0};
+  broadcast.sin_family = AF_INET;
+  broadcast.sin_addr = b_addr;
+  broadcast.sin_port = htons(PORT);
+
   lifx_frame_t frame = {
       .header =
           {
@@ -168,32 +130,102 @@ int main(void) {
       .payload = {},
   };
 
-  int sfd = start_server();
+  int sfd = socket(AF_INET, SOCK_DGRAM, 0);
   if (sfd == -1) {
-    fprintf(stderr, "failed to start server\n");
+    perror("socket");
     exit(EXIT_FAILURE);
   }
-  printf("Started server\n");
+  printf("created socket!\n");
 
-  int res = send_packet(sfd, &frame);
-  if (res == -1) {
-    fprintf(stderr, "failed to send packet\n");
-    close(sfd);
+  int yes = 1;
+  if (setsockopt(sfd, SOL_SOCKET, SO_BROADCAST, &yes, sizeof(yes)) == -1) {
+    perror("setsockopt");
     exit(EXIT_FAILURE);
   }
-  printf("Sent packet\n");
+  printf("set the broadcast flag!\n");
 
-  lifx_frame_t inboundFrame = {0};
-  lifx_frame_t *inboundFrames = &inboundFrame;
-  res = get_lights(sfd, &inboundFrames, 1);
-  if (res == -1) {
-    fprintf(stderr, "failed to get lights\n");
-    close(sfd);
+  if (bind(sfd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+    perror("bind");
     exit(EXIT_FAILURE);
   }
+  printf("bound the socket to a port!\n");
+
+  uint8_t p[FRAME_SIZE_MAX] = {0};
+  uint8_t *packet = p;
+  int size;
+  size = lifx_encode_frame(&frame, &packet, FRAME_SIZE_MAX);
+  if (size == -1) {
+    fprintf(stderr, "failed to encode lifx packet\n");
+    exit(EXIT_FAILURE);
+  }
+  printf("encoded the packet with size %d!\n", size);
+
+  if (sendto(sfd, packet, size, 0, (struct sockaddr *)&broadcast,
+             sizeof(broadcast)) != size) {
+    fprintf(stderr, "failed to broadcast packet\n");
+    exit(EXIT_FAILURE);
+  }
+  printf("broadcasted packet!\n");
+
+  struct pollfd pfds = {
+      .fd = sfd,
+      .events = POLLIN,
+  };
+
+  puts("");
+  while (1) {
+    int ready = poll(&pfds, 1, 500); /* Wait 500 miliseconds for an event */
+    if (ready == -1) {
+      perror("poll");
+      break;
+    }
+
+    if (ready == 0) {
+      break;
+    }
+
+    uint8_t inbound_p[FRAME_SIZE_MAX] = {0};
+    uint8_t *inbound_packet = inbound_p;
+    struct sockaddr_storage storage = {0};
+    socklen_t storage_len = sizeof(storage);
+    char recv_ip[INET_ADDRSTRLEN] = {0};
+    uint16_t recv_port = 0;
+
+    int size = recvfrom(sfd, inbound_packet, FRAME_SIZE_MAX, 0,
+                        (struct sockaddr *)&storage, &storage_len);
+    if (size == -1) {
+      perror("recvfrom");
+      exit(EXIT_FAILURE);
+    }
+
+    if (storage.ss_family == AF_INET) {
+      struct sockaddr_in *addr = (struct sockaddr_in *)&storage;
+      if (inet_ntop(AF_INET, &addr->sin_addr, recv_ip, INET6_ADDRSTRLEN) ==
+          NULL) {
+        perror("inet_ntop ipv4");
+        exit(EXIT_FAILURE);
+      }
+      recv_port = ntohs(addr->sin_port);
+    } else {
+      struct sockaddr_in6 *addr = (struct sockaddr_in6 *)&storage;
+      if (inet_ntop(AF_INET6, &addr->sin6_addr, recv_ip, INET6_ADDRSTRLEN) ==
+          NULL) {
+        perror("inet_ntop ipv6");
+        exit(EXIT_FAILURE);
+      }
+      recv_port = ntohs(addr->sin6_port);
+    }
+    printf("received message from %s on port %d\n", recv_ip, recv_port);
+
+    lifx_frame_t inbound_frame = {0};
+    if (lifx_decode_frame(&inbound_frame, &inbound_packet, size) == -1) {
+      fprintf(stderr, "failed to decode lifx packet\n");
+      exit(EXIT_FAILURE);
+    }
+    frame_print(&inbound_frame);
+    puts("");
+  }
+
   close(sfd);
-
-  frame_print(&inboundFrame);
-
   return 0;
 }
